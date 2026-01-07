e3b40a95fd227c8373a2d49a590d92df6416f990
Toby-A-eng <tobyroyanderson@gmail.com>
2026-01-07 11:44:30 +0000

    add app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = '6457fghd@@'
+
+DATABASE = 'database.db'
+
+# -- Initialise database
+
+def init_db():
+    with sqlite3.connect(DATABASE) as conn:
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL
+            )
+                     
+            CREATE TABLE IF NOT EXISTS staff (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL
+            )
+                     
+            CREATE TABLE IF NOT EXISTS tickets (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                customerid INTEGER NOT NULL UNIQUE
+            )
+                     
+            CREATE TABLE IF NOT EXISTS bookings (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                customerid INTEGER NOT NULL UNIQUE,
+                date DATETIME NOT NULL
+            )
+        ''') # Tickets for queue
+
+        conn.commit()
